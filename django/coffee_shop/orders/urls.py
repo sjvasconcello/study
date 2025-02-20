feeef3e460b193724a732297c9bc39151fa6c36d
sjvasconcello <sjvasconcello@gmail.com>
2025-02-20 02:25:37 -0300

    24/32 🚧 platzi django

--- django/coffee_shop/orders/urls.py
+++ django/coffee_shop/orders/urls.py
@@ -1,5 +1,7 @@
 
 from django.urls import path
+from .views import MyOderView
 
 urlpatterns = [
+    path('mi-orden/', MyOderView.as_view(), name='my_order')
 ]
--- django/coffee_shop/orders/views.py
+++ django/coffee_shop/orders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+
+from .models import Order
 
 # Create your views here.
+class MyOderView(DetailView):
+    model = Order
+    template_name = "orders/my-order.html"
+    context_object_name = "order"
+
+    def get_object(self, queryset=None):
+        return Order.objects.filter(is_active=True).first()
