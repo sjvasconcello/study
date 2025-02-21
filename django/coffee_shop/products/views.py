f72fccb8d3b3fa17c61ba49986cb9d02f7c33f4f
sjvasconcello <sjvasconcello@gmail.com>
2025-02-21 03:41:32 -0300

    27/32 🚧 platzi django

--- django/coffee_shop/products/views.py
+++ django/coffee_shop/products/views.py
@@ -1,8 +1,12 @@
 
 from django.urls import reverse_lazy
 from django.views import generic
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from .forms import ProductForm
 from .models import Product
+from .serializers import ProductSerializer
 
 class ProductFormView(generic.FormView):
     template_name = "products/add_products.html"
@@ -18,3 +22,14 @@ class ProductListView(generic.ListView):
     model = Product
     template_name = 'products/list_products.html'
     context_object_name = 'products'
+
+# Django rest framework
+class ProductListAPI(APIView):
+
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
